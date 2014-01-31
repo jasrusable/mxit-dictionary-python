0342943f339539ff6b3e6be248f95186003658b2
Jason Russell <jasrusable@gmail.com>
2014-02-01 01:16:13 +0200

    Added test with Mock synsets for lookup_word.

--- dictionary/dictionary/test_dictionary.py
+++ dictionary/dictionary/test_dictionary.py
@@ -1,13 +1,25 @@
 import unittest
 from dictionary import Dictionary
+from mock import Mock, patch
 
 class TestDictionary(unittest.TestCase):
 	def setUp(self):
 		self.dictionary = Dictionary()
 
 	def test_lookup_word(self):
-		results = self.dictionary.lookup_word('hello')
-		self.assertIsInstance(results, list)
-		for result in results:
-			self.assertIsInstance(result, basestring)
+		with patch('dictionary.dictionary.dictionary.wordnet') as wordnet:
+			mock_synsets = list()
+			definitions = ['hello', 'bye']
+			for definition in definitions:
+				mock_synset = Mock()
+				mock_synset.definition = definition
+				mock_synsets.append(mock_synset)
+			wordnet.synsets = Mock(return_value=mock_synsets)	
+			test_word = 'hello'
+			results = self.dictionary.lookup_word(test_word)
+			self.assertIsInstance(results, list)
+			for result in results:
+				self.assertIsInstance(result, basestring)
+			self.assertEqual(results, definitions)
+			wordnet.synsets.assert_called_with(test_word)
 
