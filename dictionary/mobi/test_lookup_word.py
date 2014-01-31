09f27ffa4a18b7f3d61c00c1188ed135934a43eb
Jason Russell <jasrusable@gmail.com>
2014-02-01 01:45:04 +0200

    Fixed test_lookup_word.

--- dictionary/mobi/test_lookup_word.py
+++ dictionary/mobi/test_lookup_word.py
@@ -1,5 +1,6 @@
 from flask.ext.testing import TestCase, Twill
 from dictionary import app
+from mock import Mock, patch
 
 
 class TestLookupWord(TestCase):
@@ -10,6 +11,11 @@ class TestLookupWord(TestCase):
 		return app
 
 	def test_definitions(self):
-		with Twill(self.app, port=5000) as t:
-			t.browser.go(t.url('/mobi/lookup_word?word=dog'))
-			pass
+		def check(word, defs):
+			with patch('dictionary.mobi.dictionary_instance') as mock_dictionary:
+				mock_dictionary.lookup_word = Mock(return_value=defs)
+				with Twill(self.app, port=5000) as t:
+					t.browser.go(t.url('/mobi/lookup_word?word=%s' % word))
+					mock_dictionary.lookup_word.assert_called_with(word)
+		for word, defs in [("", []), ("sdf", ["hello"])]:
+			check(word, defs)
