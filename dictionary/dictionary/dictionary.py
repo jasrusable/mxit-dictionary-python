053586da412ad15fe6095a8b59ab5b8b04deb2ed
Jason Russell <jasrusable@gmail.com>
2014-02-01 00:49:41 +0200

    Dont load dictionary in CI mode.

--- dictionary/dictionary/dictionary.py
+++ dictionary/dictionary/dictionary.py
@@ -1,12 +1,16 @@
 import nltk
 from nltk.corpus import wordnet
-
-try:
-	wordnet.synsets('word')
-except:
-	nltk.download('wordnet')
-
+import os
 
 class Dictionary(object):
+	def __init__(self):
+		if 'CI' not in os.environ:
+			try:
+				wordnet.synsets('word')
+			except:
+				nltk.download('wordnet')
+		else:
+			print ("Dictionary running in CI mode.")
+
 	def lookup_word(self, word):
 		return [synset.definition for synset in wordnet.synsets(word)]
