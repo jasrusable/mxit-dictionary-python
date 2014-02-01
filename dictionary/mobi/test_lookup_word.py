bcb8643501bef7ef9d913c9253034ea7a4099ac4
Jason Russell <jasrusable@gmail.com>
2014-02-01 14:39:09 +0200

    Test results are rendered correctly

--- dictionary/mobi/test_lookup_word.py
+++ dictionary/mobi/test_lookup_word.py
@@ -1,6 +1,8 @@
 from flask.ext.testing import TestCase, Twill
+from flask import url_for
 from dictionary import app
 from mock import Mock, patch
+from bs4 import BeautifulSoup
 
 
 class TestLookupWord(TestCase):
@@ -19,3 +21,14 @@ class TestLookupWord(TestCase):
 					mock_dictionary.lookup_word.assert_called_with(word)
 		for word, defs in [("", []), ("sdf", ["hello"])]:
 			check(word, defs)
+
+	def test_lookup_word_results(self):
+		with Twill(self.app, port=5000) as t:
+			word = 'hello'
+			test_definitions = ['a greeting', 'jasonii']
+			with patch('dictionary.mobi.dictionary_instance') as mock_dictionary:
+				mock_dictionary.lookup_word = Mock(return_value=test_definitions)
+				t.browser.go(t.url(url_for('mobi.lookup_word', word=word)))
+				soup = BeautifulSoup(t.browser.get_html())
+				results = [unicode(result_element.string.strip()) for result_element in soup.ul.find_all('li')]
+				self.assertEquals(results, test_definitions )
